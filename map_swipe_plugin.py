ecda201e52297d0d3c43760580f7eee00f754201
luolingchun <luolingchun@outlook.com>
2021-06-12 16:15:16 +0800

    release v1.1.2: add message

--- map_swipe_plugin.py
+++ map_swipe_plugin.py
@@ -7,7 +7,7 @@ import os
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtWidgets import QComboBox, QAction, QToolBar
 from qgis.core import QgsProject
-from qgis.gui import QgsMapToolPan, QgisInterface
+from qgis.gui import QgisInterface
 
 from .map_swipe_tool import MapSwipeTool
 
@@ -27,7 +27,7 @@ class MapSwipePlugin:
         self.swipe_action = QAction(QIcon(os.path.join(here, 'icon.png')), 'swipe tool', self.tool_bar)
         self.swipe_action.setCheckable(True)
         self.swipe_action.triggered.connect(self.run)
-        self.layer_combobox = QComboBox(self.tool_bar)
+        self.layer_combobox = QComboBox(self.iface.mainWindow())
         self.layer_combobox.setSizeAdjustPolicy(QComboBox.AdjustToContents)
         self.tool_bar.setContentsMargins(0, 0, 0, 0)
         height = self.iface.iconSize().height() + 8
@@ -36,7 +36,7 @@ class MapSwipePlugin:
         self.swipe_tool = MapSwipeTool(height, self.layer_combobox, self.map_canvas)
 
         # 图层变化信号
-        project.layerTreeRoot().layerOrderChanged.connect(lambda: self.combobox_add_items())
+        project.layerTreeRoot().layerOrderChanged.connect(self.combobox_add_items)
         project.layerTreeRoot().visibilityChanged.connect(self.combobox_add_items)
         project.layerTreeRoot().nameChanged.connect(self.combobox_add_items)
 
@@ -50,12 +50,14 @@ class MapSwipePlugin:
     def unload(self):
         if self.prevTool and self.prevTool != self.swipe_tool:
             self.map_canvas.setMapTool(self.prevTool)
-        else:
-            self.map_canvas.setMapTool(QgsMapToolPan(self.iface.mapCanvas()))
 
         del self.tool_bar
 
     def run(self, is_checked):
+        if self.layer_combobox.count() < 2:
+            self.swipe_action.setChecked(False)
+            self.iface.messageBar().pushMessage("At least two layers are required.")
+            return
         if is_checked:
             self.prevTool = self.map_canvas.mapTool()
             self.map_canvas.setMapTool(self.swipe_tool)
@@ -67,3 +69,6 @@ class MapSwipePlugin:
         layers = project.layerTreeRoot().checkedLayers()
         names = [layer.name() for layer in layers]
         self.layer_combobox.addItems(names)
+        if self.layer_combobox.count() < 2:
+            self.map_canvas.setMapTool(self.prevTool)
+            self.swipe_action.setChecked(False)
