600b5ebf42f2e8d017dc8495e9fe2cf73f93f862
Rackar <27681135@qq.com>
2024-08-17 10:14:22 +0800

    添加获取Windows屏幕缩放比例的方法，使比例不为100%时的卷帘正常工作

--- mapItem.py
+++ mapItem.py
@@ -5,6 +5,30 @@
 from qgis.PyQt.QtCore import QRectF, Qt, QLineF
 from qgis.PyQt.QtGui import QPen, QColor
 from qgis.gui import QgsMapCanvasItem
+import ctypes
+
+
+def get_windows_scaling_factor():
+    try:
+        # 调用 Windows API 函数获取缩放比例
+        user32 = ctypes.windll.user32
+        user32.SetProcessDPIAware()
+        scaling_factor = user32.GetDpiForSystem()
+
+        # 计算缩放比例
+        return scaling_factor / 96.0
+
+    except Exception as e:
+        print("获取缩放比例时出错:", e)
+        return None
+
+
+# 调用函数获取 Windows 桌面的缩放比例
+scaling_factor = get_windows_scaling_factor()
+if scaling_factor is not None:
+    print("Windows 桌面的缩放比例:", scaling_factor)
+else:
+    scaling_factor = 1
 
 
 class SwipeMapItem(QgsMapCanvasItem):
@@ -21,8 +45,8 @@ class SwipeMapItem(QgsMapCanvasItem):
         self.h = 0
 
     def updateImageRect(self, x, y):
-        w = self.boundingRect().width()
-        h = self.boundingRect().height()
+        w = self.boundingRect().width()*scaling_factor
+        h = self.boundingRect().height()*scaling_factor
         if self.direction == -1:  # all
             self.x = 0
             self.y = 0
@@ -32,24 +56,24 @@ class SwipeMapItem(QgsMapCanvasItem):
             self.x = 0
             self.y = 0
             self.w = w
-            self.h = y
+            self.h = y*scaling_factor
             self.line = QLineF(0, y, w, y)
         elif self.direction == 1:  # 1:'⬆'
             self.x = 0
-            self.y = y
+            self.y = y*scaling_factor
             self.w = w
-            self.h = h - y
+            self.h = h - y*scaling_factor
             self.line = QLineF(0, y, w, y)
         elif self.direction == 2:  # 2:'➡'
             self.x = 0
             self.y = 0
-            self.w = x
+            self.w = x*scaling_factor
             self.h = h
             self.line = QLineF(x, 0, x, h)
         else:  # 3:'⬅'
-            self.x = x
+            self.x = x*scaling_factor
             self.y = 0
-            self.w = w - x
+            self.w = w - x*scaling_factor
             self.h = h
             self.line = QLineF(x, 0, x, h)
         self.startPaint = True
@@ -66,5 +90,7 @@ class SwipeMapItem(QgsMapCanvasItem):
         if self.line:
             painter.drawLine(self.line)
 
-        image = self.image.copy(int(self.x), int(self.y), int(self.w), int(self.h))
-        painter.drawImage(QRectF(self.x, self.y, self.w, self.h), image)
+        image = self.image.copy(int(self.x), int(
+            self.y), int(self.w), int(self.h))
+        painter.drawImage(QRectF(self.x,
+                          self.y, self.w/scaling_factor, self.h/scaling_factor), image)
